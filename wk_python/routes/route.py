6e7a606b8245f87d2b615d33819614aa6def4bcf
marco-cst <marco.castillo@unl.edu.ec>
2024-11-02 21:49:08 -0500

    implementacion vistas

---
+++ wk_python/routes/route.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, abort, request, render_template, redirect
+import requests # type: ignore
+router = Blueprint ('router', __name__)
+
+@router.route('/')
+def home():
+    
+    return render_template('fragmento/inicial/login.html')
+
+
+##-----Ruta para acceder a los proyectos 
+@router.route('/admin/proyectos/list')
+def mt_proyectos():
+    r = requests.get('http://localhost:8080/myapp/proyectos/infoProyectos')
+    #print(type(r.json()))
+    #print(r.json())
+    data = r.json()
+    return render_template('fragmento/persona/lista.html', lista = data ["data"])
