2977817243253706cafc09bfdfeab68dbb91e33e
marco-cst <marco.castillo@unl.edu.ec>
2024-11-03 02:58:26 -0500

    Front Proyectos, Inversionistas

--- wkEDD_python/routes/route.py
+++ wkEDD_python/routes/route.py
@@ -7,8 +7,7 @@ def home():
     
     return render_template('fragmento/inicial/login.html')
 
-
-##-----Ruta para acceder a los proyectos 
+#-----Ruta para acceder a los proyectos 
 @router.route('/admin/proyectos/list')
 def mt_proyectos():
     r = requests.get('http://localhost:8088/myapp/proyectos/infoproyectos')
@@ -16,3 +15,24 @@ def mt_proyectos():
     #print(r.json())
     data = r.json()
     return render_template('fragmento/persona/lista.html', lista = data ["data"])
+
+##-----Ruta para acceder a los inversionistas
+@router.route('/admin/inversionista/lista')
+def mt_inversionistas():
+    r = requests.get('http://localhost:8088/myapp/proyectos/inversionistas')
+    data = r.json()
+    
+    inversionistas_list = extract_inversionistas(data["data"])
+    
+    print("Inversionistas List:", inversionistas_list)
+    
+    return render_template('fragmento/persona/inversionistas.html', lista=inversionistas_list)
+
+#--extraer los ivnersionistas de la lista
+def extract_inversionistas(data):
+    inversionistas = []
+    current = data.get('header')
+    while current:
+        inversionistas.append(current['info'])
+        current = current.get('next')
+    return inversionistas
